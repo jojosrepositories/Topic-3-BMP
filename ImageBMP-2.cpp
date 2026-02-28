61cc368e0dbbfe013e762124a7d1c9c6cdc39e7e
jojosrepositories <sharkboyjrh@gmail.com>
2026-02-27 23:26:40 -0600

    Add files via upload

---
+++ ImageBMP-2.cpp
@@ -0,0 +1,46 @@
+#include "ImageBMP.h"
+
+// Simple Rectangle class
+class Rectangle
+{
+private:
+    unsigned int x;
+    unsigned int y;
+    unsigned int width;
+    unsigned int height;
+    Color color;
+
+public:
+    Rectangle(unsigned int x, unsigned int y,
+        unsigned int width, unsigned int height,
+        const Color& color)
+        : x(x), y(y), width(width), height(height), color(color)
+    {
+    }
+
+    void draw(ImageBMP& image) const
+    {
+        image.fillRectangleWithColor(x, y, width, height, color);
+    }
+
+    int main()
+    {
+        ImageBMP image(500, 500, Color(ColorEnum::Black));
+        
+        //purple rectangle
+        Rectangle purpleRect
+        (50, 50, 200, 150, Color(128, 0, 128));
+
+        purpleRect.draw(image);
+
+       //white diagonal line
+        for (unsigned int i = 0; i < 500; ++i)
+        {
+            image.setPixelToColor_withThickness( i, i, Color(ColorEnum::White), 3);
+        }
+
+        image.writeImageFile("output.bmp");
+
+        return 0;
+    }
+};
